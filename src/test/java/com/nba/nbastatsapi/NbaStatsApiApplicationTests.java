426038a5ab581f874da79bcc8671f1eba29fed06
MSI\User <haozfuz@gmail.com>
2026-05-21 23:15:09 +1000

    fix: exclude datasource in test context

--- src/test/java/com/nba/nbastatsapi/NbaStatsApiApplicationTests.java
+++ src/test/java/com/nba/nbastatsapi/NbaStatsApiApplicationTests.java
@@ -2,8 +2,10 @@ package com.nba.nbastatsapi;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class NbaStatsApiApplicationTests {
 
     @Test
